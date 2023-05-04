17de0274ed6d131d47c2bd22ea7eee6f8ef0352f
Maxim Voloshin <mvolloshin@gmail.com>
2023-05-04 21:23:31 +0300

    add arm nop instruction

--- lockfree/nop.h
+++ lockfree/nop.h
@@ -1,10 +1,17 @@
+#pragma once
 
+#if defined(__X86__)
 #include <emmintrin.h>
+#endif
 
 namespace lockfree {
 
 inline void nop() {
+#if defined(__X86__)
   _mm_pause();
+#elif defined(__ARM_ARCH)
+  asm volatile("yield");
+#endif
 }
 
 }  // namespace lockfree
